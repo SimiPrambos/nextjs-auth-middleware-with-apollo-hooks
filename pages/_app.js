bb3fcf9271c8481850f81fe6bbe7f9aab67f0cd1
Simi Prambos <simi@Egas-MacBook-Pro.local>
2019-09-15 18:24:09 +0700

    change middleware to server side execution

--- pages/_app.js
+++ pages/_app.js
@@ -4,34 +4,38 @@ import React from "react"
 import withApollo from "../lib/withApollo"
 import { ApolloProvider } from "@apollo/react-hooks"
 
-function handleMiddleware(middlewares, ctx) {
-	if(middlewares) {
-		let i = 0
-		const count = middlewares.length
+async function handleMiddleware(middlewares, ctx) {
+  if (middlewares) {
+    let i = 0
+    const count = middlewares.length
 
-		function next() {
-			i++
-			if(i > count){
-				return
-			}
-			middlewares[i-1](ctx, next)
-		}
-		next()
-	}
+    async function next() {
+      i++
+      if (i > count) {
+        return
+      }
+      await middlewares[i - 1](ctx, next)
+    }
+    await next()
+  }
 }
 
 class MyApp extends App {
 
-	render() {
-		const { Component, pageProps, apolloClient, router } = this.props
-		handleMiddleware(Component.middlewares, {apolloClient, router})
+  static async getInitialProps({ ctx, Component: { middlewares } }) {
+    await handleMiddleware(middlewares, ctx)
+    return {}
+  }
 
-		return (
-			<ApolloProvider client={apolloClient}>
-				<Component {...pageProps} />
-			</ApolloProvider>
-		)
-	}
+  render() {
+    const { Component, pageProps, apolloClient, router } = this.props
+
+    return (
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    )
+  }
 }
 
 export default withApollo(MyApp)
